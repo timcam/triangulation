2dab7e655ffd08e91a09b80b2af30c656c0c502e
timcam <timcam@berkeley.edu>
2015-04-28 11:17:23 -0700

    finished triangulation?

--- readnode.py
+++ readnode.py
@@ -9,6 +9,92 @@ edgeID = 0
 quadlist = []
 
 #####################################################
+########   Delauney Trianulation       ##############
+#####################################################
+def Delaunay(s):
+ #takes a list of tuples as points
+
+ #Base case with two points
+ if len(s) == 2:
+  a = MakeEdge()
+  #select edge object
+  a[0].org  = s[0]
+  a[0].dest = s[1]
+
+  return (a, Sym(a))
+ 
+ #base case with three points
+ elif (len(s) == 3):
+  a = MakeEdge(); b = MakeEdge()
+  Splice(Sym(a),b)
+
+  a[0].org = s[0]; a[0].dest = s[1]
+  b[0].org = s[1]; b[0].dest = s[2]
+
+  if CCW(s[0],s[1],s[2]):
+   c = Connect(a,b)
+   return (a, Sym(b))
+
+  elif (s[0],s[2],s[1]):
+   c = Connect(b,a)
+   return (Sym(c), c)
+
+  else:
+   return (a, Sym(b))
+
+  #split the set again and then zip together
+ else:
+  left, right = HalfSet(s)
+
+  ldo, ldi = Delaunay(left);
+  rdi, rdo = Delaunay(right);
+
+  while True:
+  if LeftOf(rdi[0].org, ldi):
+   ldi = Lnext(ldi)
+  elif RightOf(ldi[0].org, rdi):
+   rdi = Rprev(rdi)
+  else:
+    break
+
+  #create the base edge  
+  basel = Connect(Sym(rdi), ldi)
+
+  ldo = Sym(basel) if (ldi[0].org == ldo[0].org)
+  rdo = basel if (rdi[0] == rdo[0].org)
+
+  #this is the merge loop
+  while True:
+   lcand = Onext(Sym(basel))
+   if Valid(lcand):
+    while InCircle(basel[0].dest, basel[0].org, lcand[0].dest, Onext(lcand)[0].dest):
+     t = Onext(lcand); DeleteEdge(lcand); lcand = t
+
+   # symmetrically locate first R point to hit and delete R edges
+   rcand = Oprev(basel)
+   if Valid(rcand):
+    while InCircle(basel[0].dest, basel[0].org, lcand[0].dest, Oprev(lcand)[0].dest):
+     t = Oprev(rcand); DeleteEdge(rcand); rcand = t
+
+   # if both lcand and rcand are invalid then basel L is tanget
+   if not Valid(lcand) and not Valid(rcand):
+    break
+
+   # if both are valid then choose the right edge with incircle
+   if not Valid(lcand) or (Valid(rcad) and InCircle(lcand[0].dest, lcand[0].org, rcand[0].org, rcand[0].dest)):
+    # add cross edge basel from rcand to basel.dest
+    basel = Connect(rcand, Sym(basel))
+   else:
+    basel = Connect(Sym(basel), Sym(lcand))
+
+  return (ldo, rdo)
+
+
+#map(tuple,x)
+
+
+
+#####################################################
 #############    Classes             ################
 #####################################################
  
@@ -187,10 +273,12 @@ def DeleteEdge(e):
 ##################################################
 
 ###### Right Of ########
+#takes a tuple x and quadedge e
 def RightOf(x, e):
  return CCW(x, e[0].dest, e[0].org)
 
 ###### Left OF ########
+#takes a tuple x and quadedge e
 def LeftOf(x, e):
  return CCW(x, e[0].org, e[0].dest) 
 
@@ -217,7 +305,7 @@ def Valid(e, base):
 ###    quickselect  ###
 #######################
 #http://stackoverflow.com/questions/19258457/python-quickselect-function-finding-the-median
-def quickSelect(seq,k):
+def QuickSelect(seq,k):
  k = len(seq) // 2
  # this part is the same as quick sort
  len_seq = len(seq)
@@ -241,18 +329,23 @@ def quickSelect(seq,k):
 #######################
 ###    halfSet  ###
 #######################
-def halfSet(s):
+def HalfSet(s):
 
- # for odd number of points split an even set and add right side
- if ((len(s) % 2) > 0):
-  left, right = np.vsplit(s[0:-1],2)
-  #add the extra to right side
-  right = np.vstack([right,x[-1,:]])
+ 
 
-  return left, right 
+ # # for odd number of points split an even set and add right side
+ # if ((len(s) % 2) > 0):
 
- else:
-  left, right = np.vsplit(s,2)
+ #  left, right = np.vsplit(s[0:-1],2)
+ #  #add the extra to right side
+ #  right = np.vstack([right,x[-1,:]])
+
+ #  return left, right 
+
+ # else:
+  left  = s[:len(s)/2]
+  right = s[len(s)/2:]
+  #left, right = np.vsplit(s,2)
   return left, right
 
  #median = quickSelect(s,(len(s) // 2))
@@ -262,7 +355,7 @@ def halfSet(s):
 ###########  File Handling     ################
 ###############################################
 
-def parsefile(filename):
+def ParseFile(filename):
  # Read the file and header
  f = open(filename, "r")
  header = f.readline()
@@ -295,8 +388,6 @@ def parsefile(filename):
  #tuple(vertices[1,:].tolist())
 
 
-
-
 ################################
 ### Main Arguments Statement ###
 ################################
@@ -327,7 +418,7 @@ def main(argv):
    print 'File not valid'
    sys.exit()
 
- vertices = parsefile(filename)
+ vertices = ParseFile(filename)
  print filename
 
 
